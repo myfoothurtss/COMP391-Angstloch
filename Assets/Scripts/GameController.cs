1e59c2c56073f615740c92776606525fd9b2d726
bpb <braydenpaul@live.ca>
2022-12-01 10:07:24 -0500

    Optimized gamecontroller script

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,15 +9,17 @@ public class GameController : MonoBehaviour
     public GameObject swordSwing, swordSwingSpawn;
     public float fireRate = 0.25f;
     public float timer = 0;
+    private Rigidbody2D rBody;
 
     // Start is called before the first frame update
     void Start()
     {
         playerSpeed = 1;
+        rBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontalMovement;
         float verticalMovement;
@@ -25,27 +27,29 @@ public class GameController : MonoBehaviour
         verticalMovement = Input.GetAxis("Vertical");
 
         Vector2 newVelocity = new Vector2(horizontalMovement, verticalMovement);
-        GetComponent<Rigidbody2D>().velocity = newVelocity * playerSpeed;
+        rBody.velocity = newVelocity * playerSpeed;
 
-        Vector2 moveDirection = gameObject.GetComponent<Rigidbody2D>().velocity;
+        Vector2 moveDirection = rBody.velocity;
         if (moveDirection != Vector2.zero)
         {
             float angle = Mathf.Atan2(moveDirection.x, -moveDirection.y) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         }
-
-        if (Input.GetAxis("Fire1") > 0 && timer > fireRate)
+    }
+        // Update is called once per frame
+        void Update()
         {
+            if (Input.GetAxis("Fire1") > 0 && timer > fireRate)
+            {
             //GameObject.instantiate();
             GameObject goObj;
             goObj = Instantiate(swordSwing, swordSwingSpawn.transform.position, swordSwingSpawn.transform.rotation);
             goObj.transform.Rotate(new Vector3(0, 0, 90));
             Destroy (goObj, 1);
             timer = 0;
+            }
+            timer += Time.deltaTime;
         }
-        timer += Time.deltaTime;
-
-
-    }
+        
 }
 
