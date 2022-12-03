440e1946c2cdbf4fe89c63de788366d26f20e78b
bpb <braydenpaul@live.ca>
2022-12-02 23:20:05 -0500

    Added retry button and hid sword hitbox for now. Final commit before submission

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Restart ()
+    {
+        SceneManager.LoadScene(sceneName:"AngstlochLEVEL1");
+    }
+
     public void QuitGame ()
     {
         Debug.Log ("Quit Succesfully!");
